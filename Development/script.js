b8b942ca7cfc85b147ef8f44af5a61ee112471e8
haris-aqeel <56465230+haris-aqeel@users.noreply.github.com>
2020-08-07 12:54:59 +0500

    Updated script.js
    Testing Phase 1

--- Development/script.js
+++ Development/script.js
@@ -1,15 +1,17 @@
 
 
 
-    if (document.getElementById("button").clicked == true){        
+function buttonClicked(){ 
+        alert("hjajjaj");     
         let username_value = document.getElementById("username").value;
         let email_value = document.getElementById("email").value;
         let password_value = document.getElementById("password").value;
         let password1_value = document.getElementById("password1").value;
         
         //CALLING CLASS CHECK INPUT
-        CheckInputs(username_value, email_value, password_value, password1_value);
-    }
+        myForm = new CheckInputs(username_value, email_value, password_value, password1_value);
+        myForm.checkNotEmpty();
+}
 
 class CheckInputs{
     constructor(username_values, email_values, password_values, password1_values){
@@ -19,11 +21,16 @@ class CheckInputs{
         this.password1 = password1_values;
     }
 
+    
+
     checkNotEmpty(){
         if (this.username!=="" && this.email!=="" && this.password!=="" && this.password1!==""){
+            alert("check not empty ");
+
             this.checkUsername();
             this.checkEmail();
             this.checkPassword();
+
         }
         else{
             alert("All Fields are necessary to be Filled");
@@ -31,39 +38,43 @@ class CheckInputs{
     }
 
     checkUsername(){
-        let username_length = this.username.length();
+        let username_length = (this.username).length;
         if (username_length < 4 ){
-            document.querySelector("#username").addClassName("input-error");
-            document.querySelector("small").addClassName("small-error"); //   NEEDS TO BE UPDATED LATER ON
+            document.querySelector("#username").className = "input-error" ;
+            document.querySelector("small").className = "small-error"; //   NEEDS TO BE UPDATED LATER ON
         }
     }
 
     checkEmail() {
-        let email_length = this.email.length();
+        let email_length = this.email.length;
+        let flag = false;
         for (var i = 0; i < email_length; i++){
             if (this.email[i]==='@'){
-                break;
-            }else{
-                document.querySelector("#email").addClassName("input-error");
-                document.querySelector("small").addClassName("small-error"); //   NEEDS TO BE UPDATED LATER ON
+                flag = true;   
             }
-
         }
+        
+        if (flag === false){
+            document.querySelector("#email").className = "input-error";
+            document.querySelector("small").className = "small-error"; //   NEEDS TO BE UPDATED LATER ON
+        
+        }
+        
     }
 
     checkPassword() {
-        let password_length = this.password_length;
-        if (password_length > 8 && password_length < 14){
-            if (password_value===password1_value){
-                return;
+        let password_length = this.password.length;
+        if (password_length > 8 && password_length < 18){
+            if (this.password===this.password1){
+                null;
             }else{
-                document.querySelector("#password1").addClassName("input-error");    
+                document.querySelector("#password1").className = "input-error";    
             }
-            return;
-            
+                   
         }else{
-            document.querySelector("#password").addClassName("input-error");
-            document.querySelector("small").addClassName("small-error"); //   NEEDS TO BE UPDATED LATER ON
+            document.querySelector("#password").className = "input-error";
+            document.querySelector("#password1").className = "input-error"
+            // document.querySelector("small").className = "small-error"; //   NEEDS TO BE UPDATED LATER ON
         }
     }
 
